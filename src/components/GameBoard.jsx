49ca32b8a53e719442094b223aea6a54e2c75629
kamildziuk <kamildziukwebdeveloper@gmail.com>
2025-06-18 01:02:13 +0200

    Added a jump sound when pressing a character
    Added jump sound when pressing the main character in script.js file

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,25 @@
+export default function GameBoard({ onSecectSquare, board }) {
+  return (
+    
+    <ol id="game-board">
+
+      {board.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                {" "}
+                <button
+                  onClick={() => onSecectSquare(rowIndex, colIndex)}
+                  disabled={playerSymbol !== null}
+                >
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+}
